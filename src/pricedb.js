4270a52f384026a7a00c7fc9f136cbe6d42f7970
Mark Williams <mark@readysetmark.com>
2018-05-23 22:41:00 -0500

    Quick and dirty regex for pricedb file

--- src/pricedb.js
+++ src/pricedb.js
@@ -1,11 +1,30 @@
 const readline = require('readline');
 const fs = require('fs');
 
-const rli = readline.createInterface({
-    input: fs.createReadStream(process.env.WEALTH_PULSE_PRICES_FILE),
-    crlfDelay: Infinity
-});
+// Load .pricedb file
+// remove duplicates (one time thing)
+// merge ycharts data
+// Save .pricedb file
 
-rli.on('line', line => {
-    console.log(`Line from file: ${line}`);
-});
+readPriceDb(process.env.WEALTH_PULSE_PRICES_FILE);
+
+
+function readPriceDb(priceDbFilePath) {
+    const rli = readline.createInterface({
+        input: fs.createReadStream(priceDbFilePath),
+        crlfDelay: Infinity
+    });
+    
+    let numLines = 0;
+    rli.on('line', line => {
+        const priceRegex = /^P (\d{4}-\d{2}-\d{2}) (".*"|\w*) (\$)(\d+\.\d{2})/;
+        const result = priceRegex.exec(line);
+        //console.log(`Line from file: ${line}`);
+        //console.log(`Regex result: ${result}`);
+        numLines += 1;
+    });
+
+    rli.on('close', () => {
+        console.log(`Lines read: ${numLines}`)
+    })
+}
